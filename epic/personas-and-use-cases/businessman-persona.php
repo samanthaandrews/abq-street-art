1a97cbf35b5919ab65b4478a58a1f1ce48bfb89f
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-30 13:13:32 -0700

    developing Greg's persona

--- epic/personas-and-use-cases/businessman-persona.php
+++ epic/personas-and-use-cases/businessman-persona.php
@@ -17,7 +17,12 @@
 						<li>Always has his Pixel 2 on him to respond to work and personal communication</li>
 						<li>Most likely will only interact with site on his phone</li>
 					</ul>
+				<li>Needs: Greg needs something to do in downtown ABQ as he is visiting for work. He </li>
 			</ul>
+			<h3>User Story</h3>
+			<p>As a one-time user of the site, Greg wants to quickly access the street art database and use the map function to explore downtown.</p>
+			<h3>Use Case</h3>
+			<p>Greg has finished his conference on textile socks early at the Galeria in downtown Albuquerque. He has a few hours to kill before his dinner meeting and he would like to check out the murals and art around town. He needs to be able to load the database and associated maps without much fuss. He would like to be able to make notes</p>
 		</div>
 	</body>
 </html>
