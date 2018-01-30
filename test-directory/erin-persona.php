55b0baf02082060625debc15de1b5a969ee5dd0a
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-30 11:50:25 -0700

    Nat is changing the persona

--- test-directory/erin-persona.php
+++ test-directory/erin-persona.php
@@ -5,7 +5,8 @@
 		<title>ABQ Street Art Persona</title>
 	</head>
 <body>
-	<h1>Nat is overriding this file</h1>
+	<h1>Nat is double overriding this file</h1>
+	<p>Testing again</p>
 
 </body>
 </html>
