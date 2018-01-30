5f5706b41221959713028eb7cdbbb26bf92f676d
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-30 13:04:58 -0700

    developing Greg's persona

--- epic/personas-and-use-cases/businessman-persona.php
+++ epic/personas-and-use-cases/businessman-persona.php
@@ -5,7 +5,19 @@
 		<title>Businessman Persona</title>
 	</head>
 	<body>
-
-
+		<div class="persona">
+			<h1>Persona: Young professional visiting Albquerque</h1>
+			<ul>
+				<li>Name: Greg McClusky</li>
+				<li>Gender: Male</li>
+				<li>Age: 34</li>
+				<li>Technology:</li>
+					<ul>
+						<li>Greg uses a MacBook Air and a Google Pixel 2</li>
+						<li>Always has his Pixel 2 on him to respond to work and personal communication</li>
+						<li>Most likely will only interact with site on his phone</li>
+					</ul>
+			</ul>
+		</div>
 	</body>
 </html>
