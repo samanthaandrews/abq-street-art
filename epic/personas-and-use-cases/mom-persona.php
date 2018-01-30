f0c408336ef02e0efad0d4de60a9216610a1028a
erincodes <erinleeannscott@gmail.com>
2018-01-30 13:27:43 -0700

    Added persona details for mom.

--- epic/personas-and-use-cases/mom-persona.php
+++ epic/personas-and-use-cases/mom-persona.php
@@ -6,6 +6,16 @@
 	</head>
 	<body>
 		<h1>Persona: Mom</h1>
-		<p>Content</p>
+			<p>Name: Margaret, goes by Maggie</p>
+			<p>Age: 36</p>
+			<p>Languages: English</p>
+			<p>Location: Lives on Albuquerque's westside</p>
+			<p>Technology proficiency: Moderate. As a mom on the go, Maggie primarily uses her iPhone 8 for everything. At home, their family shares a PC desktop computer. Maggie likes the convenience of her phone, but notices that as she ages, it gets harder and harder to see.</p>
+			<p>Backstory: Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
+		<h1>User Story:</h1>
+			<p></p>
+		<h1>User Story:</h1>
+			<p>Interaction Flow:</p>
 	</body>
 </html>
+
