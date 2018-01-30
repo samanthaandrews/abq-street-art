83a0ed6aa48c83becb9571a224eb3c81db8f794b
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-30 13:43:12 -0700

    added millennial persona and image

--- epic/personas/millennial-persona.php
+++ epic/personas/millennial-persona.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sam
- * Date: 1/30/18
- * Time: 11:35 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Millennial Persona</title>
+	</head>
+	<html>
+		<body>
+			<h1>Millennial Persona</h1>
+			<img src="https://static.pexels.com/photos/405041/pexels-photo-405041.jpeg" alt="millennial" width="250px"/>
+			<p>Tara Lewis</p>
+			<p>Woman, age 24</p>
+			<p>Macbook pro and iphone 7, comfortable with computers and technology, uses email, Google Drive, and various
+				social media apps</p>
+			<p>Community organizer at a local nonprofit</p>
+			<p>Tara is planning a Saturday afternoon with friends. They are going to walk around downtown ABQ and locate
+				nearby sculptures and murals to use in their selfies for Instagram and Tinder. They also plan to stop for
+				food and drinks at a bar or restaurant.</p>
+		</body>
+	</html>
+
+
+
