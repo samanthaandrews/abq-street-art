54a593f8c776005264d46b16cf3fcfa452602436
erincodes <erinleeannscott@gmail.com>
2018-01-30 13:43:45 -0700

    Added User Story, Use Case, and Interaction Flow for mom persona.

--- epic/personas/mom-persona.php
+++ epic/personas/mom-persona.php
@@ -11,11 +11,18 @@
 			<p>Languages: English</p>
 			<p>Location: Lives on Albuquerque's westside</p>
 			<p>Technology proficiency: Moderate. As a mom on the go, Maggie primarily uses her iPhone 8 for everything. At home, their family shares a PC desktop computer. Maggie likes the convenience of her phone, but notices that as she ages, it gets harder and harder to see.</p>
-			<p>Backstory: Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
 		<h1>User Story:</h1>
-			<p></p>
-		<h1>User Story:</h1>
-			<p>Interaction Flow:</p>
+			<p>As a local Albuquerque user, Maggie wants to use the application multiple times to explore public art throughout the city over an extended period of time.</p>
+		<h1>Use Case:</h1>
+			<p>Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
+		<h1>Interaction Flow:</h1>
+			<ol>
+				<li>Open mobile website/app on smart phone.</li>
+				<li>Login with username & password: Maggie wants the ability to see past experiences archive, and the ability to post photos.</li>
+				<li>Once logged in, view map based on target area (by neighborhood name or zip code)</li>
+				<li>Taps on pins on map to view different pieces of art/murals. Gets an overview of what she'd like to see and "bookmarks" the art that she's most interested in.</li>
+				<li>One Maggie has driven to the area and parked, she uses the interface to navigate to each specific location. As long as the app is open/running, the interface notifies her when she is nearby to one of the pieces of art.</li>
+			</ol>
 	</body>
 </html>
 
