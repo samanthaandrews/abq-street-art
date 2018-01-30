e4b9afd5cb13ef27cd84cb35884991ffe8d286ad
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-30 13:45:34 -0700

    developing Greg's persona

--- epic/personas/businessman-persona.php
+++ epic/personas/businessman-persona.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+persona<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
@@ -11,18 +11,18 @@
 				<li>Name: Greg McClusky</li>
 				<li>Gender: Male</li>
 				<li>Age: 34</li>
+				<li>Location: Greg is from Austin, TX and is in Albuquerque for a tech conference. THi is his second time to Albuquerque.</li>
 				<li>Technology:</li>
 					<ul>
 						<li>Greg uses a MacBook Air and a Google Pixel 2</li>
 						<li>Always has his Pixel 2 on him to respond to work and personal communication</li>
 						<li>Most likely will only interact with site on his phone</li>
 					</ul>
-				<li>Needs: Greg needs something to do in downtown ABQ as he is visiting for work. He </li>
+				<li>Backstory: Greg spends a lot of time travelling around the country doing marketing and outreach for his company, Austin Global Enterprise. He enjoys getting to know new cities via art, bars, and overall vibe. On his first visit to Albuquerque he was very disappointed by the apparent lack of atmosphere in the downtown area. </li>
+				<li>Needs: Greg needs something to do in downtown ABQ as he is visiting for work. He enjoys exploring and has an appreciation for modern art and street art.</li>
 			</ul>
 			<h3>User Story</h3>
-			<p>As a one-time user of the site, Greg wants to quickly access the street art database and use the map function to explore downtown.</p>
-			<h3>Use Case</h3>
-			<p>Greg has finished his conference on textile socks early at the Galeria in downtown Albuquerque. He has a few hours to kill before his dinner meeting and he would like to check out the murals and art around town. He needs to be able to load the database and associated maps without much fuss. He would like to be able to make notes</p>
-		</div>
+			<p>As a one-time user of the site, Greg wants to quickly access the street art database and use the map function to explore downtown. </p>
+
 	</body>
 </html>
--- epic/use-cases/businessman-use-case.php
+++ epic/use-cases/businessman-use-case.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Businessman Use Case</title>
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>ABQ Street Art Businessman Use Case</title>
+	</head>
 <body>
-<h1>The Businessman</h1>
+	<h2>Use Case</h2>
+	<p>Greg has finished his Innovate New Mexico conference early at the Galeria in downtown Albuquerque. He has a few hours to kill before his dinner meeting and he would like to check out the murals and art around town. He needs to be able to load the database and associated maps without much fuss. He would like to be able to make notes</p>
 </body>
+
 </html>
