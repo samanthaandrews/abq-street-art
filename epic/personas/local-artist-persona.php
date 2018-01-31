95217ee9423d41668d0949857874379f3c2b773a
erincodes <erinleeannscott@gmail.com>
2018-01-31 16:53:37 -0700

    Updated interaction flow for "local artist" persona.

--- epic/personas/local-artist-persona.php
+++ epic/personas/local-artist-persona.php
@@ -14,16 +14,7 @@
 		<h1>User Story:</h1>
 			<p>As a local Albuquerque artist, Maggie is always on the lookout for news ways to explore art throughout the city. She wants to discover new pieces and be able to favorite/bookmark various pieces.
 				wants to use the application multiple times to discover and explore pieces of public art throughout the city over an extended period of time.</p>
-		<h1>Use Case:</h1>
-			<p>Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
-		<h1>Interaction Flow:</h1>
-			<ol>
-				<li>Open mobile website/app on smart phone.</li>
-				<li>Login with username & password: Maggie wants the ability to see past experiences archive, and the ability to post photos.</li>
-				<li>Once logged in, view map based on target area (by neighborhood name or zip code)</li>
-				<li>Taps on pins on map to view different pieces of art/murals. Gets an overview of what she'd like to see and "bookmarks" the art that she's most interested in.</li>
-				<li>One Maggie has driven to the area and parked, she uses the interface to navigate to each specific location. As long as the app is open/running, the interface notifies her when she is nearby to one of the pieces of art.</li>
-			</ol>
+
 	</body>
 </html>
 
--- epic/use-cases/local-artist-use-case.php
+++ epic/use-cases/local-artist-use-case.php
@@ -5,6 +5,20 @@
     <title>Local Artist Use Case and Interaction Flow<</title>
 </head>
 <body>
-<h1>Local Artist Use Case and Interaction Flow</h1>
-</body>
+	<h2>Local Artist Use Case and Interaction Flow</h2>
+		<p>Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
+		<h2>Interaction Flow:</h2>
+<!--		Copy/pasted the following from the "young tourist" persona in order to match formats. It may need to be tweaked further in order to be specific to this persona. -Erin 1/31/18-->
+		<ol>
+			<li>User loads the site on her mobile device</li>
+			<li>Website asks to allow notifications and access her location</li>
+			<li>User clicks "Yes"</li>
+			<li>Website displays map with option to check out recommended routes and saved bookmarked locations</li>
+			<li>User looks at recommended routes</li>
+			<li>Website displays the various options</li>
+			<li>User chooses route of her liking</li>
+			<li>Website loads her choice</li>
+			<li>User goes on art tour!</li>
+		</ol>
+	</body>
 </html>
