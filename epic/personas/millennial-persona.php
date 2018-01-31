ba525b33e6b5c64b388aa402a01889fa406acc26
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-31 12:50:53 -0700

    edited millennial persona

--- epic/personas/millennial-persona.php
+++ epic/personas/millennial-persona.php
@@ -10,12 +10,11 @@
 			<img src="https://static.pexels.com/photos/405041/pexels-photo-405041.jpeg" alt="millennial" width="250px"/>
 			<p>Tara Lewis</p>
 			<p>Woman, age 24</p>
-			<p>Macbook pro and iphone 7, comfortable with computers and technology, uses email, Google Drive, and various
-				social media apps</p>
+			<p>Macbook pro and iphone 7, comfortable with computers and technology. Uses email, Google Drive, and various
+				social media apps. Much less likely to use an app or website with poor user experience design.</p>
 			<p>Community organizer at a local nonprofit</p>
 			<p>Tara is planning a Saturday afternoon with friends. They are going to walk around downtown ABQ and locate
-				nearby sculptures and murals to use in their selfies for Instagram and Tinder. Tara will be using her iPhone
-				7 to navigate downtown art and take photos. They also plan to stop for
+				nearby sculptures and murals to use in their selfies for Instagram and Tinder. Tara will be using her iPhone 7 to navigate downtown art and take photos. They also plan to stop for
 				food and drinks at a bar or restaurant.</p>
 		</body>
 	</html>
