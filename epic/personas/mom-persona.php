c9f096f7b3c2ca8d9dd210359063c6878b8cdc96
erincodes <erinleeannscott@gmail.com>
2018-01-31 15:06:46 -0700

    Updating latest version.

--- epic/personas/mom-persona.php
+++ epic/personas/mom-persona.php
@@ -10,9 +10,10 @@
 			<p>Age: 36</p>
 			<p>Languages: English</p>
 			<p>Location: Lives on Albuquerque's westside</p>
-			<p>Technology proficiency: Moderate. As a mom on the go, Maggie primarily uses her iPhone 8 for everything. At home, their family shares a PC desktop computer. Maggie likes the convenience of her phone, but notices that as she ages, it gets harder and harder to see.</p>
+			<p>Technology proficiency: Moderate. As an artist that is often on-location for projects, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she mostly uses to watch Netflix.</p>
 		<h1>User Story:</h1>
-			<p>As a local Albuquerque user, Maggie wants to use the application multiple times to explore public art throughout the city over an extended period of time.</p>
+			<p>As a local Albuquerque artist, Maggie is always on the lookout for news ways to explore art throughout the city. She wants to discover new pieces and be able to favorite/bookmark various pieces.
+				wants to use the application multiple times to discover and explore pieces of public art throughout the city over an extended period of time.</p>
 		<h1>Use Case:</h1>
 			<p>Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
 		<h1>Interaction Flow:</h1>
