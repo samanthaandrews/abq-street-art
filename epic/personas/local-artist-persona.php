af92785d278086f258892f3b7157bf623e82e18d
erincodes <erinleeannscott@gmail.com>
2018-01-31 17:03:32 -0700

    Updated details for "local artist" persona.

--- epic/personas/local-artist-persona.php
+++ epic/personas/local-artist-persona.php
@@ -2,19 +2,17 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Artist Persona</title>
+		<title>Local Artist Persona</title>
 	</head>
 	<body>
-		<h1>Persona: Artist</h1>
-			<p>Name: Margaret, goes by Maggie</p>
+		<h1>Local Artist Persona</h1>
+			<p>Name: Maggie</p>
 			<p>Age: 36</p>
 			<p>Languages: English</p>
 			<p>Location: Lives on Albuquerque's westside</p>
-			<p>Technology proficiency: Moderate. As an artist that is often on-location for projects, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she mostly uses to watch Netflix.</p>
+			<p>Technology proficiency: Moderate. As an artist that is often on-location for projects and on-the-go, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she mostly uses to watch Netflix.</p>
 		<h1>User Story:</h1>
-			<p>As a local Albuquerque artist, Maggie is always on the lookout for news ways to explore art throughout the city. She wants to discover new pieces and be able to favorite/bookmark various pieces.
-				wants to use the application multiple times to discover and explore pieces of public art throughout the city over an extended period of time.</p>
-
+			<p>As an artist herself, Maggie would be an early-adopter of this application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to come back and bring others over time.</p>
 	</body>
 </html>
 
