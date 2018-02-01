fa440f43dec04818178635f3f6b1d0f057d9c1f4
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-31 20:18:25 -0700

    fixed typo

--- epic/personas/local-artist-persona.php
+++ epic/personas/local-artist-persona.php
@@ -14,7 +14,7 @@
 			<p>Location: Lives in Corrales, New Mexico </p>
 			<p>Technology Proficiency: Moderate. As an artist that is often on-location for projects and on-the-go, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she uses for for sketching and watching Netflix.</p>
 		<h1>User Story:</h1>
-			<p>As sn artist who lives locally, Maggie is an early-adopter of our application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to return on numerous occasions over time with others.</p>
+			<p>As an artist who lives locally, Maggie is an early-adopter of our application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to return on numerous occasions over time with others.</p>
 		<br>
 	</body>
 </html>
