15ad247a1d16d5ae5ee1dea3cbb05be5278712af
erincodes <erinleeannscott@gmail.com>
2018-02-01 15:55:39 -0700

    Commented out mentions of "recommended routes" functionality

--- epic/use-cases/local-artist-use-case.php
+++ epic/use-cases/local-artist-use-case.php
@@ -7,18 +7,18 @@
 <body>
 	<h1>Local Artist Use Case and Interaction Flow</h1>
 	<h2>Use Case:</h2>
-		<p>As a recurring user, Maggie wants to login to the system, create bookmarked art sites, and utilize the "suggested" routes functionality.</p>
+		<p>As a recurring user, Maggie wants to login to the system, create bookmarked art sites, and comment on art images.</p>
 		<h2>Interaction Flow:</h2>
 		<ol>
 			<li>User loads the site on her mobile device</li>
 			<li>Website displays the map of Albuquerque with all of the art locations</li>
 			<li>User clicks the "login" box to sign in to her account</li>
 			<li>Website verifies credentials and signs user in</li>
-			<li>User clicks the "recommended routes" that are populated on the site </li>
-			<li>Website displays the various route options</li>
-			<li>User chooses route of her liking</li>
-			<li>Website displays preferred route</li>
-			<li>User walks route and bookmarks favorite sites to return to again in the future</li>
+<!--			<li>User clicks the "recommended routes" that are populated on the site </li>-->
+<!--			<li>Website displays the various route options</li>-->
+<!--			<li>User chooses route of her liking</li>-->
+<!--			<li>Website displays preferred route</li>-->
+			<li>User bookmarks favorite sites to return to again in the future</li>
 			<li>Website saves bookmarks under user's login</li>
 			<li>User prompts the system to log out</li>
 			<li>Website securely logs user out </li>
