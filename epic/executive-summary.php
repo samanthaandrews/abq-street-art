58c2e6dc688242eedd33b643c38d58f4153cbe51
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-01 09:07:03 -0700

    created executive summary file in the epic directory and added link to the index file

---
+++ epic/executive-summary.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>ABQ Street Art Capstone</title>
+	</head>
+	<header>
+		<!--Do we need this information in the nav? I feel like it's redundant as it's below in the bullet points as well. Commenting out for now, open to push back. -Erin 1/31-->
+		<!--<nav><a href="epic/personas/local-artist-persona.php" target="_blank">Local Artist Persona</a> <a href="epic/personas/young-tourist-persona.php" target="_blank">Young Tourist Persona</a> </nav>-->
+	</header>
+	<body>
+		<h2>Executive Summary:</h2>
+		<p>ABQ Street Art is a mobile-first website that allows users to explore public art in Downtown Albuquerque. By integrating with the Google Maps API and pulling information about artwork from the <a href="http://coagisweb.cabq.gov/arcgis/rest/services/public/PublicArt/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson" target="_blank"> Public Art Database,</a> users will be able to learn more about each piece of art and its artist. Users can also bookmark locations, comment on artwork photos, and opt-in to receive notifications when they are near a piece of public art.</p>
+		<h3>Key Features:</h3>
+		<ul>
+			<li>Mobile-first web application used by prolific and novice art enthusiasts </li>
+			<li>Base interface features a map of public art locations marked with pins</li>
+			<li>Pins are clickable and provide more information: photo, artist, date created, etc.</li>
+			<li>Users are able to bookmark/favorite pieces of art</li>
+			<li>Users are able to "opt-in" to receive notifications when they are nearby to art that they have expressed bookmarked/favorited</li>
+			<li>We are hoping to include 1-2 predetermined/suggested routes that users can utilize (but not create themselves)</li>
+		</ul>
+		<br>
+		<h3>System Goals:</h3>
+
+	</body>
+</html>
