652869cb83fa1a669583259a99297c9a16f33b48
erincodes <erinleeannscott@gmail.com>
2018-01-31 20:00:56 -0700

    Updated formatting of personas to be consistent, made a few copy edits.

--- epic/personas/local-artist-persona.php
+++ epic/personas/local-artist-persona.php
@@ -2,18 +2,20 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Local Artist Persona</title>
+		<title>Local Artist</title>
 	</head>
 	<body>
-		<h1>Local Artist Persona:</h1>
+		<h1>Local Artist Persona</h1>
 		<img src="http://www.graphicdesigndegreehub.com/wp-content/uploads/2016/02/Wangechi-Mutu-influential-women-artists-alive.jpg" alt="Local Artist" width="300px"/>
-			<p>Name: Maggie</p>
+			<p>Name: Maggie Green</p>
+			<p>Gender: Female</p>
 			<p>Age: 36</p>
 			<p>Languages: English</p>
-			<p>Location: Lives in Albuquerque, near Downtown </p>
-			<p>Technology proficiency: Moderate. As an artist that is often on-location for projects and on-the-go, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she mostly uses to watch Netflix.</p>
+			<p>Location: Lives in Corrales, New Mexico </p>
+			<p>Technology Proficiency: Moderate. As an artist that is often on-location for projects and on-the-go, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she uses for for sketching and watching Netflix.</p>
 		<h1>User Story:</h1>
-			<p>As an artist herself, Maggie would be an early-adopter of this application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to come back and bring others over time.</p>
+			<p>As sn artist who lives locally, Maggie is an early-adopter of our application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to return on numerous occasions over time with others.</p>
+		<br>
 	</body>
 </html>
 
--- epic/personas/young-tourist-persona.php
+++ epic/personas/young-tourist-persona.php
@@ -2,27 +2,20 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Casual User</title>
+		<title>Young Tourist</title>
 	</head>
 	<body>
-		<div class="persona">
-			<h1>Young Tourist Persona:</h1>
-			<img src="https://previews.123rf.com/images/maridav/maridav1404/maridav140400425/27940374-man-on-smartphone-young-business-man-talking-on-smart-phone-casual-urban-professional-businessman-us.jpg" alt="Young Tourist" width="300px"/>
-			<ul>
-				<li>Name: Greg McClusky</li>
-				<li>Gender: Male</li>
-				<li>Age: 29</li>
-				<li>Location: Greg is from Austin, TX and is in Albuquerque for a tech conference. This is his second time to Albuquerque and is looking for some new, interesting things to do.</li>
-				<li>Technology:</li>
-					<ul>
-						<li>Greg uses a MacBook Air and a Google Pixel 2</li>
-						<li>Always has his Pixel 2 on him to respond to work and personal communication</li>
-						<li>Most likely will only interact with site on his phone</li>
-					</ul>
-				<li>Backstory: Greg spends a lot of time travelling around the country doing marketing and outreach for his company, Austin Global Enterprise. He enjoys getting to know new cities via art, bars, and overall vibe. On his first visit to Albuquerque he was very disappointed by the apparent lack of atmosphere in the downtown area. </li>
-				<li>Needs: Greg needs something to do in downtown ABQ as he is visiting for work. He enjoys exploring and has an appreciation for modern art and street art. Greg needs this site as a datable for the interesting art that can be found around Albuquerque.</li>
-			</ul>
-			<h3>User Story</h3>
-			<p>As a one-time user of the site, Greg wants to quickly access the street art database and use the map function to and predetermined routes to explore downtown.</p>
+			<h1>Young Tourist Persona</h1>
+				<img src="https://previews.123rf.com/images/maridav/maridav1404/maridav140400425/27940374-man-on-smartphone-young-business-man-talking-on-smart-phone-casual-urban-professional-businessman-us.jpg" alt="Young Tourist" width="300px"/>
+				<p>Name: Greg McClusky</p>
+				<p>Gender: Male</p>
+				<p>Age: 29</p>
+				<p>Languages: English</p>
+				<p>Location: Lives in Austin, Texas </p>
+				<p>Technology Proficiency: High. Greg uses a MacBook Air and always has his Google Pixel 2 on him to respond to work and personal communication.</p>
+			<h1>User Story:</h1>
+				<p>Greg spends a lot of time traveling around the country doing marketing and outreach for his company, Austin Global Enterprise. He is visiting Albuquerque for a tech conference. He enjoys getting to know new cities via art, bars, and overall vibe. Greg is staying Downtown and is looking for something to do between work commitments at the Convention Center. As a one-time user of the site, Greg wants to access the map on his phone to walk and explore nearby public art.
+			</p>
+		<br>
 	</body>
 </html>
