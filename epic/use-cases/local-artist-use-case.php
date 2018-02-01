f9f74b8597339e714f7ec0de69dea1af1a624fb7
erincodes <erinleeannscott@gmail.com>
2018-02-01 13:31:31 -0700

    Updated interaction flow for local artist persona

--- epic/use-cases/local-artist-use-case.php
+++ epic/use-cases/local-artist-use-case.php
@@ -8,17 +8,21 @@
 	<h2>Local Artist Use Case and Interaction Flow</h2>
 		<p>Maggie has always an appreciation for art, especially large outdoor installations. As an art lover, she wants to expose her two elementary-aged children to art an an early age. She is also a part-time teacher and has the idea of having her students explore Downtown Albuquerque's public art as a field trip.</p>
 		<h2>Interaction Flow:</h2>
-<!--		Copy/pasted the following from the "young tourist" persona in order to match formats. It may need to be tweaked further in order to be specific to this persona. -Erin 1/31/18-->
 		<ol>
 			<li>User loads the site on her mobile device</li>
-			<li>Website asks to allow notifications and access her location</li>
-			<li>User clicks "Yes"</li>
-			<li>Website displays map with option to check out recommended routes and saved bookmarked locations</li>
-			<li>User looks at recommended routes</li>
-			<li>Website displays the various options</li>
+			<li>Website displays the map of Albuquerque with all of the art locations</li>
+			<li>User clicks the "login" box to sign in to her account</li>
+			<li>Website verifies credentials and signs user in</li>
+			<li>User clicks the "recommended routes" that are populated on the site </li>
+			<li>Website displays the various route options</li>
 			<li>User chooses route of her liking</li>
-			<li>Website loads her choice</li>
-			<li>User goes on art tour!</li>
+			<li>Website displays preferred route</li>
+			<li>User walks route and bookmarks favorite sites to return to again in the future</li>
+			<li>Website saves bookmarks under user's login</li>
+			<li>User prompts the system to log out</li>
+			<li>Website securely logs user out </li>
+			<li>User returns to interface again in future</li>
 		</ol>
 	</body>
 </html>
+
