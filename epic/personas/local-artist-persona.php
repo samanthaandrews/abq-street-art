0893be002dc1ebbfc462ef43615a300134b72718
Mary <schmitt.mary7@gmail.com>
2018-02-01 16:47:42 -0700

    added line to persona

--- epic/personas/local-artist-persona.php
+++ epic/personas/local-artist-persona.php
@@ -13,7 +13,7 @@
 			<p>Languages: English</p>
 			<p>Location: Lives in Corrales, New Mexico </p>
 			<p>Technology Proficiency: Moderate. As an artist that is often on-location for projects and on-the-go, Maggie primarily uses her iPhone 8 for everything. At home, she also has an iPad that she uses for for sketching and watching Netflix.</p>
-			<p>As an artist who lives locally, Maggie is an early-adopter of our application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to return on numerous occasions over time with others.</p>
+			<p>As an artist who lives locally, Maggie is an early-adopter of our application. She is actively engaged in the Albuquerque arts community and is always on the lookout for new ways to explore art throughout the city. Maggie would like to be able to bookmark her favorite pieces in order to return on numerous occasions over time with others. She would also like to be able to comment her thoughts about the locations.</p>
 		<br>
 	</body>
 </html>
