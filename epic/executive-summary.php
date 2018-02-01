52aea0747f01419ec1e8a216bc9b3d518ad289f3
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-01 13:00:29 -0700

    editing html text in executive summary

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -4,13 +4,9 @@
 		<meta charset="UTF-8">
 		<title>ABQ Street Art Capstone</title>
 	</head>
-	<header>
-		<!--Do we need this information in the nav? I feel like it's redundant as it's below in the bullet points as well. Commenting out for now, open to push back. -Erin 1/31-->
-		<!--<nav><a href="epic/personas/local-artist-persona.php" target="_blank">Local Artist Persona</a> <a href="epic/personas/young-tourist-persona.php" target="_blank">Young Tourist Persona</a> </nav>-->
-	</header>
 	<body>
 		<h2>Executive Summary:</h2>
-		<p>ABQ Street Art is a mobile-first website that allows users to explore public art in Downtown Albuquerque. By integrating with the Google Maps API and pulling information about artwork from the <a href="http://coagisweb.cabq.gov/arcgis/rest/services/public/PublicArt/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson" target="_blank"> Public Art Database,</a> users will be able to learn more about each piece of art and its artist. Users can also bookmark locations, comment on artwork photos, and opt-in to receive notifications when they are near a piece of public art.</p>
+		<p>ABQ Street Art is a mobile-first website that allows users to explore public art in Downtown Albuquerque. By integrating with the Google Maps API and pulling information about artwork from the <a href="http://coagisweb.cabq.gov/arcgis/rest/services/public/PublicArt/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson" target="_blank"> Public Art Database,</a> users will be able to learn more about each piece of art and its artist. Users can also bookmark locations, comment on artwork photos, and opt-in to receive notifications when they are near a piece of public art. If we have time remaining after implementing these key features, we will also include one or two predetermined routes that users can follow.</p>
 		<h3>System Goals:</h3>
 		<h3>Key Features:</h3>
 		<ul>
@@ -19,6 +15,9 @@
 			<li>Pins are clickable and provide more information: photo, artist, date created, etc.</li>
 			<li>Users are able to bookmark/favorite pieces of art</li>
 			<li>Users are able to "opt-in" to receive notifications when they are nearby to art that they have expressed bookmarked/favorited</li>
+		</ul>
+			<h4>Stretch Goal:</h4>
+		<ul>
 			<li>We are hoping to include 1-2 predetermined/suggested routes that users can utilize (but not create themselves)</li>
 		</ul>
 	</body>
