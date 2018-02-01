f9ea86dad78783e3b1e6e2b4cc319cda588cfb34
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-01 09:11:00 -0700

    edited html

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -11,6 +11,7 @@
 	<body>
 		<h2>Executive Summary:</h2>
 		<p>ABQ Street Art is a mobile-first website that allows users to explore public art in Downtown Albuquerque. By integrating with the Google Maps API and pulling information about artwork from the <a href="http://coagisweb.cabq.gov/arcgis/rest/services/public/PublicArt/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson" target="_blank"> Public Art Database,</a> users will be able to learn more about each piece of art and its artist. Users can also bookmark locations, comment on artwork photos, and opt-in to receive notifications when they are near a piece of public art.</p>
+		<h3>System Goals:</h3>
 		<h3>Key Features:</h3>
 		<ul>
 			<li>Mobile-first web application used by prolific and novice art enthusiasts </li>
@@ -20,6 +21,5 @@
 			<li>Users are able to "opt-in" to receive notifications when they are nearby to art that they have expressed bookmarked/favorited</li>
 			<li>We are hoping to include 1-2 predetermined/suggested routes that users can utilize (but not create themselves)</li>
 		</ul>
-		<h3>System Goals:</h3>
 	</body>
 </html>
