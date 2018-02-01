ed42186355df4ec80b8729df564cf458403e2cae
Mary <schmitt.mary7@gmail.com>
2018-02-01 09:15:50 -0700

    updated with conceptual model

---
+++ epic/conceptual-model.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Conceptual Model</title>
+</head>
+<body>
+    <h1>ABQ Art Walk Conceptual Model</h1>
+    <h3>Art</h3>
+    <ul>
+        <li>artId</li>
+        <li>artArtist</li>
+        <li>artAddress</li>
+        <li>artYear</li>
+        <li>artTitle</li>
+        <li>artLocation</li>
+        <li>artX</li>
+        <li>artY</li>
+        <li>artType</li>
+        <li>artImageUrl</li>
+        <li>artJpgUrl</li>
+    </ul>
+    <h3>Profile</h3>
+    <ul>
+        <li>profileId</li>
+        <li>profileActivationToken (possibly not required)</li>
+        <li>profileEmail</li>
+        <li>profileFullName</li>
+        <li>profileHash (possibly not required)</li>
+        <li>profileSalt (possibly not required)</li>
+    </ul>
+    <h3>Bookmark</h3>
+    <ul>
+        <li>artId</li>
+        <li>profileId</li>
+    </ul>
+    <h3>Comment</h3>
+    <ul>
+        <li>commentId (is this a try hard?)</li>
+        <li>commentArtId</li>
+        <li>commentProfileId</li>
+        <li>commentContent</li>
+        <li>commentDateTime</li>
+    </ul>
+</body>
+</html>
