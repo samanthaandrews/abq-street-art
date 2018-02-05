8b6ad9d764fe213b1dbbb437bcc0422e8309313d
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-05 16:15:17 -0700

    update comment method complete https://app.asana.com/0/543064250979532/546016585496142

--- classes/Comment.php
+++ classes/Comment.php
@@ -46,136 +46,136 @@ class Comment implements \JsonSerializable {
 	private $commentDateTime;
 
 
-/**
- * constructor for this Comment
- *
- * @param string|Uuid $newCommentId id of this Comment or null if a new Comment
- * @param string|Uuid $newCommentArtId id of the Art piece the comment was made on
- * @param string|Uuid $newCommentProfileId id of the Profile that made the Comment
- * @param string $newCommentContent string containing actual Comment data
- * @param \DateTime|string|null $newCommentDateTime date and time when Comment was sent or null if set to current date and time
- * @throws \InvalidArgumentException if data types are not valid
- * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
- * @throws \TypeError if data types violate type hints
- * @throws \Exception if some other exception occurs
- * @Documentation https://php.net/manual/en/language.oop5.decon.php
- **/
-public function __construct($newCommentId, $newCommentArtId, $newCommentProfileId, string $newCommentContent, $newCommentDateTime = null) {
-	try {
-		$this->setCommentId($newCommentId);
-		$this->setCommentArtId($newCommentArtId);
-		$this->setCommentProfileId($newCommentProfileId);
-		$this->setCommentContent($newCommentContent);
-		$this->setCommentDateTime($newCommentDateTime);
-	}
-		//determine what exception type was thrown
-	catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
-	}
-}
-
-/**
- * accessor method for comment id
- *
- * @return Uuid value of comment id
- **/
-public function getCommentId() : Uuid {
-	return($this->commentId);
-}
-
-/**
- * mutator method for the comment id
- *
- * @param Uuid | string $newCommentId new value of comment id
- * @throws \RangeException if $newCommentId is not positive
- * @throws \TypeError if $newCommentId is not a uuid or string
- **/
-public function setCommentId($newCommentId) : void {
-	try {
-		$uuid = self::validateUuid($newCommentId);
-	} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	/**
+	 * constructor for this Comment
+	 *
+	 * @param string|Uuid $newCommentId id of this Comment or null if a new Comment
+	 * @param string|Uuid $newCommentArtId id of the Art piece the comment was made on
+	 * @param string|Uuid $newCommentProfileId id of the Profile that made the Comment
+	 * @param string $newCommentContent string containing actual Comment data
+	 * @param \DateTime|string|null $newCommentDateTime date and time when Comment was sent or null if set to current date and time
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function __construct($newCommentId, $newCommentArtId, $newCommentProfileId, string $newCommentContent, $newCommentDateTime = null) {
+		try {
+			$this->setCommentId($newCommentId);
+			$this->setCommentArtId($newCommentArtId);
+			$this->setCommentProfileId($newCommentProfileId);
+			$this->setCommentContent($newCommentContent);
+			$this->setCommentDateTime($newCommentDateTime);
+		} //determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 	}
 
-	//convert and store the comment id
-	$this->commentId = $uuid;
-}
-
-/**
- * accessor method for comment art id
- *
- * @return Uuid value of comment art id
- **/
-public function getCommentArtId() : Uuid{
-	return($this->CommentArtId);
-}
-
-/**
- * mutator method for comment art id
- *
- * @param string | Uuid $newCommentArtId new value of comment art id
- * @throws \RangeException if $newCommentArtId is not positive
- * @throws \TypeError if $newCommentArtId is not an integer
- **/
-public function setCommentArtId( $newCommentArtId) : void {
-	try {
-		$uuid = self::validateUuid($newCommentArtId);
-	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	/**
+	 * accessor method for comment id
+	 *
+	 * @return Uuid value of comment id
+	 **/
+	public function getCommentId(): Uuid {
+		return ($this->commentId);
 	}
 
-	// convert and store the profile id
-	$this->CommentArtId = $uuid;
-}
-/**
- * accessor method for comment profile id
- *
- * @return Uuid value of comment profile id
- **/
-public function getCommentProfileId() : Uuid{
-	return($this->commentProfileId);
-}
+	/**
+	 * mutator method for the comment id
+	 *
+	 * @param Uuid | string $newCommentId new value of comment id
+	 * @throws \RangeException if $newCommentId is not positive
+	 * @throws \TypeError if $newCommentId is not a uuid or string
+	 **/
+	public function setCommentId($newCommentId): void {
+		try {
+			$uuid = self::validateUuid($newCommentId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 
-/**
- * mutator method for comment profile id
- *
- * @param string | Uuid $newCommentProfileId new value of tweet profile id
- * @throws \RangeException if $newCommentProfileId is not positive
- * @throws \TypeError if $newCommentProfileId is not an integer
- **/
-public function setCommentProfileId( $newCommentProfileId) : void {
-	try {
-		$uuid = self::validateUuid($newCommentProfileId);
-	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		//convert and store the comment id
+		$this->commentId = $uuid;
 	}
 
-	// convert and store the profile id
-	$this->CommentProfileId = $uuid;
-}
+	/**
+	 * accessor method for comment art id
+	 *
+	 * @return Uuid value of comment art id
+	 **/
+	public function getCommentArtId(): Uuid {
+		return ($this->CommentArtId);
+	}
 
-/**
- * accessor method for comment content
- *
- * @return string value of comment content
- **/
-public function getCommentContent() : string {
-	return($this->commentContent);
-}
+	/**
+	 * mutator method for comment art id
+	 *
+	 * @param string | Uuid $newCommentArtId new value of comment art id
+	 * @throws \RangeException if $newCommentArtId is not positive
+	 * @throws \TypeError if $newCommentArtId is not an integer
+	 **/
+	public function setCommentArtId($newCommentArtId): void {
+		try {
+			$uuid = self::validateUuid($newCommentArtId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 
-/**
- * mutator method for comment content
- *
- * @param string $newCommentContent new value of the comment content
- * @throws \InvalidArgumentException if $newCommentContent is not a string or insecure
- * @throws \RangeException if $newCommentContent is > 4096 characters
- * @throws \TypeError if $newCommentContent is not a string
- **/
-public function setCommentContent() : string {
+		// convert and store the profile id
+		$this->CommentArtId = $uuid;
+	}
+
+	/**
+	 * accessor method for comment profile id
+	 *
+	 * @return Uuid value of comment profile id
+	 **/
+	public function getCommentProfileId(): Uuid {
+		return ($this->commentProfileId);
+	}
+
+	/**
+	 * mutator method for comment profile id
+	 *
+	 * @param string | Uuid $newCommentProfileId new value of tweet profile id
+	 * @throws \RangeException if $newCommentProfileId is not positive
+	 * @throws \TypeError if $newCommentProfileId is not an integer
+	 **/
+	public function setCommentProfileId($newCommentProfileId): void {
+		try {
+			$uuid = self::validateUuid($newCommentProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		// convert and store the profile id
+		$this->CommentProfileId = $uuid;
+	}
+
+	/**
+	 * accessor method for comment content
+	 *
+	 * @return string value of comment content
+	 **/
+	public function getCommentContent(): string {
+		return ($this->commentContent);
+	}
+
+	/**
+	 * mutator method for comment content
+	 *
+	 * @param string $newCommentContent new value of the comment content
+	 * @throws \InvalidArgumentException if $newCommentContent is not a string or insecure
+	 * @throws \RangeException if $newCommentContent is > 4096 characters
+	 * @throws \TypeError if $newCommentContent is not a string
+	 **/
+	public function setCommentContent(string $newTweetContent): void {
 		// verify the comment content is secure
 		$newCommentContent = trim($newCommentContent);
 		$newCommentContent = filter_var($newCommentContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -193,78 +193,98 @@ public function setCommentContent() : string {
 	}
 
 
-/**
- * accessor method for comment date time
- *
- * @return \DateTime value of comment date
- **/
-public function getCommentDateTime() : \DateTime {
-	return($this->commentDateTime);
-	}
-/**
- * mutator method for the comment date time
- *
- * @param \DateTime|string|null $newCommentDateTime comment date as a DateTime object or string (or null to load the current time)
- * @throws \InvalidArgumentException if $newCommentDateTime is not a valid object or string
- * @throws \RangeException if $newCommentDateTime is a date that does not exist
- **/
-public function setCommentDateTime($newCommentDateTime = null) : void {
-	// base case: if the date is null, use the current date and time
-	if($newCommentDateTime === null) {
-		$this->commentDateTime = new \DateTime();
-		return;
+	/**
+	 * accessor method for comment date time
+	 *
+	 * @return \DateTime value of comment date
+	 **/
+	public function getCommentDateTime(): \DateTime {
+		return ($this->commentDateTime);
 	}
 
-	// store the comment date using the ValidateDate trait
-	try {
-		$newCommentDateTime = self::validateDateTime($newCommentDateTime);
-	} catch(\InvalidArgumentException | \RangeException $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	/**
+	 * mutator method for the comment date time
+	 *
+	 * @param \DateTime|string|null $newCommentDateTime comment date as a DateTime object or string (or null to load the current time)
+	 * @throws \InvalidArgumentException if $newCommentDateTime is not a valid object or string
+	 * @throws \RangeException if $newCommentDateTime is a date that does not exist
+	 **/
+	public function setCommentDateTime($newCommentDateTime = null): void {
+		// base case: if the date is null, use the current date and time
+		if($newCommentDateTime === null) {
+			$this->commentDateTime = new \DateTime();
+			return;
+		}
+
+		// store the comment date using the ValidateDate trait
+		try {
+			$newCommentDateTime = self::validateDateTime($newCommentDateTime);
+		} catch(\InvalidArgumentException | \RangeException $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		$this->commentDateTime = $newCommentDateTime;
 	}
-	$this->commentDateTime = $newCommentDateTime;
+
+	/**
+	 * inserts this Comment into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo): void {
+
+		// create query template
+		$query = "INSERT INTO comment(commentId, commentArtId, commentProfileId, commentContent, commentDateTime) VALUES(:commentId, :commentArtId,:commentProfileId, :commentContent, :contentDateTime)";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders in the template
+		$formattedDate = $this->commentDateTime > format("Y-m-d H:i:s.u");
+		$parameters = ["commentId" => $this->commentId->getBytes(), "commentArtId" => $this->commentArtId->getBytes(), "commentProfileId" => $this->commentProfileId->getBytes(), "commentContent" => $this->commentContent, "commentDateTime" => $formattedDate];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * deletes this Comment from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo): void {
+
+		// create query template
+		$query = "DELETE FROM comment WHERE commentId = :commentId";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holder in the template
+		$parameters = ["commentId" => $this->commentId->getBytes()];
+		$statement->execute($parameters);
+	}
+
+
+	/**
+	 * updates this Comment in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related error occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo) : void {
+
+		//create a query template
+		$query = "UPDATE comment SET commentArtId = :commentArtId, commentProfileId = :commentProfileId, commentContent = :commentContent, commentDateTime = :commentDateTime WHERE commentId = :commentId";
+		$statement = $pdo->prepare($query);
+
+		$formattedDate = $this->commentDateTime->("Y-m-d H:i:s.u");
+		$parameters = ["commentId" => $this->commentId->getBytes(), "commentArtId"=>$this->commentArtId->getBytes(), "commentProfileId"=>$this->commentProfileId->getBytes(), "commentContent"=>$this->commentContent, "commentDateTime"=>$formattedDate];
+		$statement->execute($parameters);
+	}
+
 }
 
-/**
- * inserts this Comment into mySQL
- *
- * @param \PDO $pdo PDO connection object
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError if $pdo is not a PDO connection object
- **/
-public function insert(\PDO $pdo) : void {
 
-	// create query template
-	$query = "INSERT INTO comment(commentId, commentArtId, commentProfileId, commentContent, commentDateTime) VALUES(:commentId, :commentArtId,:commentProfileId, :commentContent, :contentDateTime)";
-	$statement = $pdo->prepare($query);
-
-	// bind the member variables to the place holders in the template
-	$formattedDate = $this->commentDateTime>format("Y-m-d H:i:s.u");
-	$parameters = ["commentId" => $this->commentId->getBytes(), "commentArtId" => $this->commentArtId->getBytes(), "commentProfileId" => $this->commentProfileId->getBytes(), "commentContent" => $this->commentContent, "commentDateTime" => $formattedDate];
-	$statement->execute($parameters);
-}
-
-/**
- * deletes this Comment from mySQL
- *
- * @param \PDO $pdo PDO connection object
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError if $pdo is not a PDO connection object
- **/
-public function delete(\PDO $pdo) : void {
-
-	// create query template
-	$query = "DELETE FROM comment WHERE commentId = :commentId";
-	$statement = $pdo->prepare($query);
-
-	// bind the member variables to the place holder in the template
-	$parameters = ["commentId" => $this->commentId->getBytes()];
-	$statement->execute($parameters);
-}
-
-
-
-}}
 
 
 /**
