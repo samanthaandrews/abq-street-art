bc4af56961a3363146bd4050598c82b7b0e01590
Mary <schmitt.mary7@gmail.com>
2018-02-06 16:39:42 -0700

    updated ProfileTest file
    https://app.asana.com/0/543064250979532/543106786642584

--- classes/Test/ProfileTest.php
+++ classes/Test/ProfileTest.php
@@ -5,3 +5,24 @@
  * Date: 2/6/2018
  * Time: 4:13 PM
  **/
+
+namespace Edu\Cnm\AbqStreetArt\Test;
+
+use Edu\Cnm\AbqStreetArt\Profile;
+
+
+require_once (dirname(__DIR__) . "/autoload.php");
+
+//uuid generator goes here but I don't know what that is so I'm not gonna add it TODO add uuid generator
+
+/**
+ * PHPUnit test for ABQ Street Art Profile class
+ *
+ *
+ * @see \Edu\Cnm\AbqStreetArt\Profile
+ * @author Mary MacMillan <mschmitt5@cnm.edu>
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ **/
+class ProfileTest extends DataDesignTest {
+
+}
