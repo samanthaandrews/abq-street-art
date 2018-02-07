c302ce64e33c6f9a6908f5de48525ceb8b7708d0
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-07 11:59:30 -0700

    small debugging of comment class https://app.asana.com/0/543064250979532/546016585496142

--- classes/Test/CommentTest.php
+++ classes/Test/CommentTest.php
@@ -9,7 +9,7 @@ require_once (dirname(__DIR__) . "/autoload.php");
 //uuid generator goes here but I don't know what that is so I'm not gonna add it TODO add uuid generator
 
 /**
- * PHPUnit test for ABQ Street Art Profile class
+ * PHPUnit test for ABQ Street Art Comment class
  *
  *
  * @see \Edu\Cnm\AbqStreetArt\Profile
@@ -18,9 +18,17 @@ require_once (dirname(__DIR__) . "/autoload.php");
  **/
 
 class CommentTest extends StreetArtTest {
-
 	/**
+	 * Art that the comment is about; this is for foreign keys relations
 	 *
+	 * @var Art art
 	 **/
-	protected $
+	protected $art = null;
+	/**
+	 *Profile that created the comment; this is for foreign key relations
+	 * @var Profile profile
+	 * Prolifle is needed to make a comment, so we use it but set it to null
+	 */
+	protected $profile = null;
+
 }
