8e214be848506668672c351c4f747b20ac31ad84
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-07 12:54:46 -0700

    set up parameters for the CommentTest https://app.asana.com/0/543064250979532/546016585496142

--- classes/Test/CommentTest.php
+++ classes/Test/CommentTest.php
@@ -18,17 +18,69 @@ require_once (dirname(__DIR__) . "/autoload.php");
  **/
 
 class CommentTest extends StreetArtTest {
+
 	/**
 	 * Art that the comment is about; this is for foreign keys relations
 	 *
 	 * @var Art art
 	 **/
 	protected $art = null;
+
 	/**
 	 *Profile that created the comment; this is for foreign key relations
 	 * @var Profile profile
-	 * Prolifle is needed to make a comment, so we use it but set it to null
+	 * Profile is needed to make a comment, so we use it but set it to null
 	 */
 	protected $profile = null;
 
+	/**
+	 * valid profile hash to create the profile object to own the test
+	 * @var $VALID_HASH
+	 */
+	protected $VALID_PROFILE_HASH;
+	/**
+	 * valid salt to use to create the profile object to own the test
+	 * @var string $VALID_SALT
+	 */
+	protected $VALID_PROFILE_SALT;
+
+	/**
+	 * content of the Comment
+	 * @var string $VALID_COMMENTCONTENT
+	 **/
+	protected $VALID_COMMENTCONTENT = "The PHPUnit test is passing for this comment";
+
+	/**
+	 * content of the updated Comment
+	 * @var string $VALID_COMMENTCONTENT2
+	 **/
+	protected $VALID_COMMENTCONTENT2 = "The PHPUnit test is still passing for this comment";
+
+	/**
+	 ** This is the timestamp of the comment; this starts as null and is assigned later
+	 **/
+	protected $VALID_COMMENTDATETIME = null;
+
+	/**
+	 * Valid timestamp to use as sunriseCommentDate
+	 **/
+	protected $VALID_SUNRISEDATE = null;
+	/**
+	 * Valid timestamp to use as sunsetCommentDate
+	 **/
+
+	protected $VALID_SUNSETDATE = null;
+	/**
+	 * create dependent objects before running each test
+	 **/
+
+	/**
+	 * create dependent objects before running each test
+	 */
+	public final function setUp() : void {
+		//run the default setUp() method first
+		parent::setUp();
+		$password = "abc123";
+
+	}
 }
