387be08a80c188ef09161c3fb955bb4fa920e761
erincodes <erinleeannscott@gmail.com>
2018-02-07 11:49:45 -0700

    Added in a few comments to the parent Unit Test https://app.asana.com/0/543064247470536/546016585496143

--- classes/Test/StreetArtTest.php
+++ classes/Test/StreetArtTest.php
@@ -5,8 +5,11 @@ use PHPUnit\DbUnit\TestCaseTrait;
 use PHPUnit\DbUnit\DataSet\QueryDataSet;
 use PHPUnit\DbUnit\Database\Connection;
 use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
+
 // grab the encrypted properties file
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+// autoload Composer packages
 require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 /**
  * Abstract class containing universal and project specific mySQL parameters
