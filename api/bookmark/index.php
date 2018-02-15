7120c98af5705529f1d3c8192d3a5a1c948c4f47
erincodes <erinleeannscott@gmail.com>
2018-02-15 11:32:04 -0700

    Pulled in George's example code to start writing API for Bookmark class.
    https://app.asana.com/0/543064247470536/560174819056351

---
+++ api/bookmark/index.php
@@ -0,0 +1,116 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+use Edu\Cnm\DataDesign\{
+	Like
+};
+/**
+ * Api for the Bookmark class
+ *
+ * @author george kephart
+ * @author Erin Scott
+ */
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	//sanitize the search parameters
+	$likeProfileId = $id = filter_input(INPUT_GET, "likeProfileId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$likeTweetId = $id = filter_input(INPUT_GET, "likeTweetId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+		//gets  a specific like associated based on its composite key
+		if ($likeProfileId !== null && $likeTweetId !== null) {
+			$like = Like::getLikeByLikeTweetIdAndLikeProfileId($pdo, $likeProfileId, $likeTweetId);
+			if($like!== null) {
+				$reply->data = $like;
+			}
+			//if none of the search parameters are met throw an exception
+		} else if(empty($likeProfileId) === false) {
+			$like = Like::getLikeByLikeProfileId($pdo, $likeProfileId)->toArray();
+			if($like !== null) {
+				$reply->data = $like;
+			}
+			//get all the likes associated with the tweetId
+		} else if(empty($likeTweetId) === false) {
+			$like = Like::getLikeByLikeTweetId($pdo, $likeTweetId)->toArray();
+			if($like !== null) {
+				$reply->data = $like;
+			}
+		} else {
+			throw new InvalidArgumentException("incorrect search parameters ", 404);
+		}
+	} else if($method === "POST" || $method === "PUT") {
+		//decode the response from the front end
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+		if(empty($requestObject->likeProfileId) === true) {
+			throw (new \InvalidArgumentException("No Profile linked to the Like", 405));
+		}
+		if(empty($requestObject->likeTweetId) === true) {
+			throw (new \InvalidArgumentException("No tweet linked to the Like", 405));
+		}
+		if(empty($requestObject->likeDate) === true) {
+			$requestObject->LikeDate =  date("y-m-d H:i:s");
+		}
+		if($method === "POST") {
+			//enforce that the end user has a XSRF token.
+			verifyXsrf();
+			//enforce the end user has a JWT token
+			//validateJwtHeader();
+			// enforce the user is signed in
+			if(empty($_SESSION["profile"]) === true) {
+				throw(new \InvalidArgumentException("you must be logged in too like posts", 403));
+			}
+			//validateJwtHeader();
+			$like = new Like($_SESSION["profile"]->getProfileId(), $requestObject->likeTweetId);
+			$like->insert($pdo);
+			$reply->message = "liked tweet successful";
+		} else if($method === "PUT") {
+			//enforce the end user has a XSRF token.
+			verifyXsrf();
+			//enforce the end user has a JWT token
+			//validateJwtHeader();
+			//grab the like by its composite key
+			$like = Like::getLikeByLikeTweetIdAndLikeProfileId($pdo, $requestObject->likeProfileId, $requestObject->likeTweetId);
+			if($like === null) {
+				throw (new RuntimeException("Like does not exist"));
+			}
+			//enforce the user is signed in and only trying to edit their own like
+			if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $like->getLikeProfileId()) {
+				throw(new \InvalidArgumentException("You are not allowed to delete this tweet", 403));
+			}
+			//validateJwtHeader();
+			//preform the actual delete
+			$like->delete($pdo);
+			//update the message
+			$reply->message = "Like successfully deleted";
+		}
+		// if any other HTTP request is sent throw an exception
+	} else {
+		throw new \InvalidArgumentException("invalid http request", 400);
+	}
+	//catch any exceptions that is thrown and update the reply status and message
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+// encode and return reply to front end caller
+echo json_encode($reply);
