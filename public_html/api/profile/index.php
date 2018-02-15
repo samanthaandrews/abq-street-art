39caa60f837469e2a691cb2fe76b14d7ebf05c4b
Mary <schmitt.mary7@gmail.com>
2018-02-15 12:24:12 -0700

    started profile api
    https://app.asana.com/0/543064250979532/560174819056346

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -1,4 +1,37 @@
 <?php
 
-require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
-require_once (dirname(__DIR__, ))
+require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once (dirname(__DIR__, 3). "/php/classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+
+//require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\AbqStreetArt\ {
+    Profile
+};
+
+/**
+ * API for profile
+ *
+ * @author Mary MacMillan <mschmitt5@cnm.edu>
+ * @author Gkephart
+ **/
+
+//verify the session, if it is not active, start it
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+//prepare empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+    //get mySQL connection
+    $pdo = connectToEncryptedMySQL("etc/apache2/capstone-mysql/streetart.ini");
+
+    //determine which HTTP method was used
+}
