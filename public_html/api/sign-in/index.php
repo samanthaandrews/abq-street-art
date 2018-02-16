6a65128fc219c2f1bb67c14ab6bb7d3b9ac27738
erincodes <erinleeannscott@gmail.com>
2018-02-16 11:36:12 -0700

    Refined sign in code and updated system response messages.
    https://app.asana.com/0/543064247470536/560174819056355

--- public_html/api/sign-in/index.php
+++ public_html/api/sign-in/index.php
@@ -3,7 +3,8 @@
 <!--Hash and salt the password and save it upon sign in. Compare this sign in password vs. sign up.-->
 <!--Hashes are just verifiers - want verifier to be the same upon sign up and sign in-->
 <!--We don’t need/want to store the actual value of the password-->
-<!--Only accepts post. Cannot get, put, or delete-->
+
+<!--Only accepts POST. Cannot GET, PUT, or DELETE-->
 <!--only takes username and password-->
 <!--grab by username, recalculate hash and if they match, they typed the password correctly and successfully sign in-->
 <!--unhelpful in error messages when throwing exceptions - "username not found" means we are open to attackers - "invalid username or password" is good. don’t have attacker any info.-->
@@ -42,61 +43,76 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 try {
+
 	//check the session status. If it is not active, start the session.
 	if(session_status() !== PHP_SESSION_ACTIVE) {
 		session_start();
 	}
+
 	//grab the database connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/rlewis37.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/streetart.ini");
+
 	//determine which HTTP method, store the result in $method
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	if($method === "POST") {
-		//var_dump($http_response_header);
+
 		//check xsrf token
 		verifyXsrf();
+
 		//grab request content, decode json into a php object
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
+
 		//check for password (required field)
 		if(empty($requestObject->profilePassword) === true) {
-			throw (new \InvalidArgumentException("No password? U shall not pass!", 401));
+			throw (new \InvalidArgumentException("A password must be entered.", 401));
 		} else {
 			$profilePassword = $requestObject->profilePassword;
 		}
+
 		//check for email (required field)
 		if(empty($requestObject->profileEmail) === true) {
-			throw (new \InvalidArgumentException("Enter an email address.", 401));
+			throw (new \InvalidArgumentException("An email address must be entered.", 401));
 		} else {
 			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
 		}
+
 		//grab the profile by email address
 		$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
 		if(empty($profile) === true) {
-			throw (new \RuntimeException("Email or password is incorrect!", 401));
+			throw (new \RuntimeException("Invalid username or password.", 401));
 		}
-		//hash the password given by user
+
+		//hash the password provided by the user
 		$hash = hash_pbkdf2("sha512", $profilePassword, $profile->getProfileSalt(), 262144);
-		//check if the hash matches what's in mysql
+
+		//check if the password hash matches what is in MySQL
 		if($hash !== $profile->getProfileHash()) {
-			throw (new \InvalidArgumentException("Email or password is incorrect!", 401));
+			throw (new \InvalidArgumentException("Invalid username or password.", 401));
 		}
-		//grab profile by id from mysql and put into the session
+
+		//grab profile by profileId from MySQL and put into the session
 		$profile = Profile::getProfileByProfileId($pdo, $profile->getProfileId());
-		//check if user has activated their acct yet
+
+		//check if user still has an outstanding activation token. User must validate token before signing in.
 		if(!empty($profile->getProfileActivationToken()) || $profile->getProfileActivationToken() !== null) {
 			throw (new \RuntimeException("Please check your email to activate your account before logging in.", 403));
 		}
+
 		//add profile to session
 		$_SESSION["profile"] = $profile;
+
 		//create the auth payload
 		$authObject = (object) [
 			"profileId" => $profile->getProfileId(),
 			"profileUsername" => $profile->getProfileUsername()
 		];
+
 		//create & set the JWT
 		setJwtAndAuthHeader("auth", $authObject);
+
 		//update reply
-		$reply->message = "Welcome! Sign in successful :D";
+		$reply->message = "Welcome! Sign in successful.";
 	} else {
 		throw (new \InvalidArgumentException("Invalid HTTP request!"));
 	}
@@ -104,7 +120,9 @@ try {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
+
 //sets up the response header
 header("Content-type: application/json");
-//finally - JSON encode the $reply object and echo it back to the front end.
+
+//lastly, JSON encode the $reply object and echo it back to the front end.
 echo json_encode($reply);
