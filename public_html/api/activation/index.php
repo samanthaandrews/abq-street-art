4bc00141c03a6046dfe88f99ef5b1ecef760746b
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-19 13:25:55 -0700

    added require once https://app.asana.com/0/543064250979532/546016585496142

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -4,6 +4,9 @@ require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__,3)."/php/classes/autoload.php";
 require_once dirname(__DIR__,3)."/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 
 use Edu\Cnm\AbqStreetArt\ {
 	Profile
