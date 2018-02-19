43ed1eb14cb4c7d4b0390cb90a2c14f57795ef1d
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-19 14:00:06 -0700

    added swiftmailer to composer.json

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -24,7 +24,7 @@ $reply->data = null;
 try {
 
     //grab the mySQL connection
-    $pdo = connectToEncryptedMySQL("etc/apache2/capstone-mysql/streetart.ini");
+    $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/streetart.ini");
 
     //determine which HTTP method was used
     $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
