5f87070df52932cdf1af294a3f26fd7a6ecef37f
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-20 15:23:35 -0700

    updated sign-up with Tristan's fixes to the bootcamp code https://app.asana.com/0/543064250979532/546016585496142

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -109,6 +109,7 @@ EOF;
         $recipients = [$requestObject->profileEmail];
 
         //set the recipient to the swift message
+		 $swiftMessage->setTo($recipients);
         //attach the subject line to the email message
         $swiftMessage->setSubject($messageSubject);
 
@@ -143,10 +144,10 @@ EOF;
          * so, if the number attempted is not the number accepted, this is an Exception
          **/
         //TODO do we want this? George has it commented out?
-        /**if($numSent !== count($recipients)) {
+        if($numSent !== count($recipients)) {
             // the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
             throw(new RuntimeException("unable to send email", 400));
-        } **/
+        }
 
         // update reply
         $reply->message = "Thank you for creating a profile with ABQ Street Art!";
