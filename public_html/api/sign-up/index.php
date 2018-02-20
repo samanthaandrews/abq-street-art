01873671548f95cda544ab8549ed784824e2c5c4
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-20 15:40:58 -0700

    updated sign-up with Tristan's fixes to the bootcamp code https://app.asana.com/0/543064250979532/546016585496142

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-require_once dirname(__DIR__, 3) . "php/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 use Edu\Cnm\AbqStreetArt\Profile;
 /**
  * api for signing up for ABQ Street Art
@@ -109,7 +109,8 @@ EOF;
         $recipients = [$requestObject->profileEmail];
 
         //set the recipient to the swift message
-		 $swiftMessage->setTo($recipients);
+		  $swiftMessage->setTo($recipients);
+
         //attach the subject line to the email message
         $swiftMessage->setSubject($messageSubject);
 
