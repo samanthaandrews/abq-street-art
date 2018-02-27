987db5cf7fbc5416f9b98454088f467177bd00e9
Samantha Andrews <samantharaeandrews@gmail.com>
2018-02-27 15:48:47 -0700

    added navbar angular template

---
+++ static-ui/map-view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sam
+ * Date: 2/27/18
+ * Time: 1:50 PM
+ */
---
+++ static-ui/navbar.php
@@ -0,0 +1,28 @@
+<header>
+	<div class="container pb-5">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<span href="home-view.php" class="navbar-brand">ABQ Street Art</span>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+					  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+					  aria-label="Toggle Navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a href="sign-up.php" class="nav-link">Sign up</a>
+					</li>
+					<li class="nav-item">
+						<a href="sign-in-modal.php" class="nav-link">Sign in</a>
+					</li>
+					<li class="nav-item">
+						<a href="update-profile.php" class="nav-link">Settings</a>
+					</li>
+					<li class="nav-item">
+						<a href="home-view.php" class="nav-link">Sign out</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</header>
