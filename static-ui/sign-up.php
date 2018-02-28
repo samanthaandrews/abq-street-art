decedc3a15339c7bf407cec440babce29711b68f
Mary <schmitt.mary7@gmail.com>
2018-02-28 13:48:01 -0700

    made sign-up static ui

--- static-ui/sign-up.php
+++ static-ui/sign-up.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sam
- * Date: 2/27/18
- * Time: 1:48 PM
- */
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<main class="gray">
+    <div class="container text-center">
+        <h1>Sign Up for ABQ Street Art!</h1>
+        <form class="form-control-lg" id="form" action="" method="post">
+            <div class="info">
+                <p>
+                    <input id="name" type="text" name="name" placeholder=" User Name"/>
+                </p>
+                <p>
+                    <input id="email" type="email" name="email" placeholder=" Email"/>
+                </p>
+                <p>
+                    <input id="password" type="text" name="password" placeholder=" Password">
+                </p>
+                <p>
+                    <input id="password-confirm" type="text" name="password-confirm" placeholder=" Re-enter Password">
+                </p>
+                <input class="btn btn-outline-warning" type="submit" value="Sign Up!">
+            </div>
+        </form>
+    </div>
+</main>
