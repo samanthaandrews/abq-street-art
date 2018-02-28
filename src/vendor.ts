438b029af3d38c9523df26de9e0ea51ab4008716
Mary <schmitt.mary7@gmail.com>
2018-02-28 09:07:56 -0700

    added src directory and main, polyfills, and vendor files

---
+++ src/vendor.ts
@@ -0,0 +1,22 @@
+// Angular 5
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
+
+// Other vendors for example Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import "popper.js";
+import "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// font awesome
+import "@fortawesome/fontawesome";
+import "@fortawesome/fontawesome-free-brands";
+import "@fortawesome/fontawesome-free-regular";
+import "@fortawesome/fontawesome-free-solid";
