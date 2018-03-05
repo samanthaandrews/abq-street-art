2748c4af20bb178bf9fdedb0d78aa3d35124432b
Samantha Andrews <samantharaeandrews@gmail.com>
2018-03-05 16:40:41 -0700

    added guard directory and sign-up, sign-in services and classes

---
+++ src/app/shared/classes/sign.up.ts
@@ -0,0 +1,7 @@
+export class SignUp {
+	constructor(public profileEmail: string,
+					public profileUserName: string,
+					public profilePassword: string,
+					public profilePasswordConfirm: string) {
+	}
+}
---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,25 @@
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+
+@Injectable()
+
+export class AuthService {
+
+
+	private token : string = localStorage.getItem("jwt-token");
+
+	constructor(private jwtHelperService: JwtHelperService, private http: HttpClient) {}
+
+	//token : string = this.jwtHelperService.tokenGetter();
+	public isAuthenticated(): boolean {
+		const token = localStorage.getItem('jwt-token');
+		console.log("I lose");
+
+		console.log(!this.jwtHelperService.isTokenExpired(token));
+		// Check whether the token is expired and return
+		// true or false
+		return !this.jwtHelperService.isTokenExpired(token);
+	}
+}
