8a9dfa05274102884fa445c599ba98d709aa61b5
Samantha Andrews <samantharaeandrews@gmail.com>
2018-03-05 16:24:45 -0700

    added interceptor typescript directory

--- src/app/shared/classes/comment.ts
+++ src/app/shared/classes/comment.ts
@@ -1,3 +1,4 @@
+//TODO I literally have no idea why this isn't recognizing these state variables?
 export class Comment {
-    constructor(public id: string, public commentArtId: string, public commentProfileId: string, public commentContent: string, public commentDateTime: string) {}
+	constructor(public id: string, public commentArtId: string, public commentProfileId: string, public commentContent: string, public commentDateTime: string) {}
 }
