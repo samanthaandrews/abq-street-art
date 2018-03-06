9afcac5b63e6ba62ca23c755b2f073af5dc65f21
Mary <schmitt.mary7@gmail.com>
2018-03-06 09:18:32 -0700

    added navbar and sign in components to shared

---
+++ src/app/shared/components/navbar.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    template: require("./navbar.component.html"),
+    selector: "navbar"
+})
+
+export class NavbarComponent {}
---
+++ src/app/shared/components/sign-in-modal.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    template: require("./sign-in-modal.component.html"),
+    selector: "signIn"
+})
+
+export class SignInModalComponent {}
