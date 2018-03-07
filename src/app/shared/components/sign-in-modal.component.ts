d13c410c5f715efb7b4543583a22210ef156bc9d
Mary <schmitt.mary7@gmail.com>
2018-03-07 09:47:09 -0700

    sign in modal works! we guessed right!

--- src/app/shared/components/sign-in-modal.component.ts
+++ src/app/shared/components/sign-in-modal.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     template: require("./sign-in-modal.component.html"),
-    selector: "signIn"
+    selector: "sign-in"
 })
 
 export class SignInModalComponent {}
