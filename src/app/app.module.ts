73cd8f235cddd65ec9b72982a12e3d3488c08c45
Mary <schmitt.mary7@gmail.com>
2018-03-07 13:27:50 -0700

    added signin to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-    imports:      [BrowserModule, HttpClientModule, routing],
+    imports:      [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule, routing],
     declarations: [...moduleDeclarations, ...allAppComponents],
     bootstrap:    [AppComponent],
     providers:    [...appRoutingProviders]
