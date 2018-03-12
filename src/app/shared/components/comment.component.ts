c35cd81f148c6192ded0a22bd1126e82a0e5f37c
Nathaniel Gustafson <natjgus@gmail.com>
2018-03-12 10:02:49 -0600

    working on the comment component.

--- src/app/shared/components/comment.component.ts
+++ src/app/shared/components/comment.component.ts
@@ -1,6 +1,40 @@
 import {Component, OnInit} from "@angular/core";
 import {Art} from "../classes/art";
-import
-import {ArtService} from "../shared/services/art.service";
-import {Status} from "../shared/classes/status";
+import {Profile} from"../classes/profile"
+import {Comment} from"../classes/comment"
+import {ArtService} from "../services/art.service";
+import {ProfileService} from "../services/profile.service";
+import {CommentService} from "../services/comment.service";
+import {Status} from "../classes/status";
 import {ActivatedRoute, Params} from "@angular/router";
+
+@Component({
+	template: require("./comment.component.html"),
+	selector: "comment"
+})
+
+export class CommentComponent implements OnInit {
+
+	comment: Comment = new Comment(null, null, null, null, null);
+
+	comments: Comment[] = [];
+	status: Status = null;
+
+	constructor(
+		private commentService: CommentService,
+		private route: ActivatedRoute,
+	) {}
+
+	ngOnInit() : void {
+	}
+
+	getComment() : void{
+		let artId : string  = this.route.snapshot.params["artId"];
+
+		this.commentService.getCommentByCommentArtId(artId)
+			.subscribe(comment => {
+				this.comment = comment;
+			});
+	}
+
+}
